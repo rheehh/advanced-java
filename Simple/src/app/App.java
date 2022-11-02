71f365f3c1d34008cbefebc01736f5f6a21124d5
rheehh <rheehh@naver.com>
2022-11-02 12:52:33 +0900

    new one

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -2,6 +2,6 @@ package app;
 
 class App{
 public static void main(String[] args){
-System.out.println("Hello World!!!!~rheehh  ?!");
+System.out.println("Hello World!!!!~rheehh for Hakhyun Lee.");
 }
 }
