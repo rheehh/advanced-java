631d907669518fd56eac1f80d858f6c75a845ec8
rheehh <rheehh@naver.com>
2022-11-04 10:30:29 +0900

    Just Once

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -2,6 +2,6 @@ package app;
 
 class App{
 public static void main(String[] args){
-System.out.println("Hello World!!!!~rheehh for Hakhyun Lee. update");
+System.out.println("Hello World!!!!~rheehh for Hakhyun Lee. update Okay");
 }
 }
